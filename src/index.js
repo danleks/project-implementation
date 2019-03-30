fe3558d842a7801b61fdbcdc00eb7d668184415c
iamdeveloper <danlex@localhost.localdomain>
2019-03-30 12:21:27 +0100

    style fixes for menu

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './style/main.scss';
 import toggleMenu from './script/toggleMenu';
 
-
 const close = document.querySelector('.nav__close');
 const burger = document.querySelector('.burger');
 close.addEventListener('click', toggleMenu);
