831772ffae38e9059f1648a98c0e39444b614dce
iamdeveloper <danlex@localhost.localdomain>
2019-04-01 19:56:56 +0200

    style fixes

---
+++ README.md
@@ -0,0 +1,4 @@
+PRACA DOMOWA No. 2
+
+## Links:
+* [GH-PAGES](https://danleks.github.io/project-implementation/)
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './style/main.scss';
 import toggleMenu from './script/toggleMenu';
-
 const close = document.querySelector('.nav__close');
 const burger = document.querySelector('.burger');
+
 close.addEventListener('click', toggleMenu);
 burger.addEventListener('click', toggleMenu);
